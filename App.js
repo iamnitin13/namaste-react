3895068dbc86ff7b670104057ae0a6b01d2cf02e
Nitin Srivastava <nitins1bro@gmail.com>
2023-04-12 15:17:51 +0530

    docs: jsx building basic

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 // importing the module or package the we have install (react and react-dom);
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom/client"; //earlier it was from react-dom
 
 /**
@@ -60,19 +60,36 @@ const HeadingComponent = () => (
 
 // this is component composition (composing multiple component into a single component)
 const ComponentCompostion = () => (
-  <div id="container">
-    <HeadingComponent />
-    <p id="paragraph">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis
-      dolorum consequuntur, quam ratione veritatis consequatur adipisci sapiente
-      minus recusandae tenetur, mollitia veniam id tempora aperiam pariatur
-      maxime nihil officiis?
-    </p>
-  </div>
+  <Fragment>
+    <>
+      <div id="container" className="app-container">
+        {jsxHeading}
+        {console.log("Hello from console")}
+        {HeadingComponent()}
+        <HeadingComponent />
+        <HeadingComponent></HeadingComponent>
+        <p id="paragraph">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis
+          dolorum consequuntur, quam ratione veritatis consequatur adipisci
+          sapiente minus recusandae tenetur, mollitia veniam id tempora aperiam
+          pariatur maxime nihil officiis?
+        </p>
+        {100 + 2000}
+      </div>
+      <div id="container-2">
+        <h2>Hello from container 2</h2>
+      </div>
+    </>
+  </Fragment>
+  // fragment and <> are the same; fragment are imported from React & <> are from jsx
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<ComponentCompostion />); // <HeadingComponnet/> is treated as jsx element
+root.render(<HeadingComponent />);
 
 // render it will take the react element i,e the js object create the tag and put in the root elment;
 // that we created using reactdom.createroot
+
+// const secondRoot = ReactDOM.createRoot(document.getElementById("root-2"));
+// secondRoot.render(<HeadingComponent />);
