093aa7842e900deaec4c5e343ce395da24ddef52
Nitin Srivastava <nitins1bro@gmail.com>
2023-04-20 01:18:09 +0530

    revert: push main.jsx for ep3 assignment

---
+++ Main.jsx
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+import Header from "./src/components/Header/Header";
+
+/**
+ *
+ *
+ * HEADER
+ *  - logo
+ *  - searchbar
+ *  - profile logo
+ */
+
+const Main = () => {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+};
+
+export default Main;
