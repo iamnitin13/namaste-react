68d9cf74a1e54b7ae2cb796fcf1a12817bd50410
Nitin Srivastava <nitins1bro@gmail.com>
2023-04-23 05:23:52 +0530

    feat: add filter functionality to food-app

--- src/FoodApp/src/components/Body/Body.jsx
+++ src/FoodApp/src/components/Body/Body.jsx
@@ -1,32 +1,89 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import ResturantCard from "../ResturantCard/ResturantCard";
-import resturants from "../../json/resturants.json";
+import resturantList from "../../json/resturants.json";
 import "./body.scss";
 
 const Body = () => {
   const btnRef = useRef();
+  const inputRef = useRef();
+  const [resturants, setResturants] = useState([...resturantList.cards]);
 
   return (
     <div className="body">
-      <div className="res-search">
-        <input
-          type="search"
-          name="resturant"
-          placeholder="Type Resturant Name..."
-          onChange={(e) =>
-            e.target.value.length
-              ? (btnRef.current.style.display = "#e36313cc")
-              : (btnRef.current.style.color = "#c5c0c0c8")
-          }
-        />
-        <button className="submit" type="submit" ref={btnRef}>
-          Search
-        </button>
+      <div className="res-selections-container ">
+        <div className="res-search">
+          <input
+            type="search"
+            name="resturant"
+            ref={inputRef}
+            placeholder="Type Resturant Name..."
+            onChange={(e) =>
+              e.target.value.length
+                ? (btnRef.current.style.color = "#e36313cc")
+                : (btnRef.current.style.color = "#c5c0c0c8")
+            }
+          />
+          <button
+            className="submit"
+            type="submit"
+            ref={btnRef}
+            onClick={() =>
+              setResturants(
+                resturants.filter((card) =>
+                  card?.data?.name
+                    .toLowerCase()
+                    .includes(inputRef.current.value.toLowerCase())
+                )
+              )
+            }
+          >
+            Search
+          </button>
+        </div>
+        <div className="res-filter">
+          <button
+            className="filter-btn"
+            onClick={() =>
+              setResturants([
+                ...resturants.sort(
+                  (cardA, cardB) =>
+                    cardB?.data?.avgRating - cardA?.data?.avgRating
+                ),
+              ])
+            }
+          >
+            Sort: Top to Low
+          </button>
+          <button
+            className="filter-btn"
+            onClick={() =>
+              setResturants([
+                ...resturants.sort(
+                  (cardA, cardB) =>
+                    cardA?.data?.avgRating - cardB?.data?.avgRating
+                ),
+              ])
+            }
+          >
+            Sort: Low to Top
+          </button>
+          <button
+            className="filter-btn reset-btn"
+            onClick={() => {
+              setResturants([...resturantList.cards]);
+              inputRef.current.value = null;
+              btnRef.current.style.color = "#c5c0c0c8";
+            }}
+          >
+            RESET
+          </button>
+        </div>
       </div>
+
       <div className="res-container">
         {/* resturant card separate component as it resuable */}
-        {resturants.cards.map(({ data: card }) => {
-          return <ResturantCard card={card} key={card.id} />;
+        {resturants.map(({ data: card }) => {
+          return <ResturantCard card={card} key={card?.id} />;
         })}
       </div>
     </div>
