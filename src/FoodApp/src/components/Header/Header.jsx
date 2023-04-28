8469f79bd5266ac763586397ebd6bfd1225e2444
Nitin Srivastava <nitins1bro@gmail.com>
2023-04-28 13:54:00 +0530

    feat: add login logut mini functionality

--- src/FoodApp/src/components/Header/Header.jsx
+++ src/FoodApp/src/components/Header/Header.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import { LOGO_URL } from "../../utils/constants";
 import "./header.scss";
 
+// const isLoggedIn = () => {
+//   //TODO: make API call to check if user is authenticated;
+//   return true;
+// };
+
 const Header = () => {
+  const [loggedIn, setloggedIn] = useState(true);
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -15,6 +23,19 @@ const Header = () => {
           <li>Cart</li>
         </ul>
       </div>
+
+      {loggedIn ? (
+        <button className="logout-btn" onClick={() => setloggedIn(false)}>
+          <div className="logout-icon">
+            <div />
+          </div>
+          Logout
+        </button>
+      ) : (
+        <button className="login-btn" onClick={() => setloggedIn(true)}>
+          Login
+        </button>
+      )}
     </div>
   );
 };
